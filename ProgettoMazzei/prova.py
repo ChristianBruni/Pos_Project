4e5a66e71e3ebd23143c923b928f49f045c8b435
ChristianBruni <christian.bruni@edu.unito.it>
2025-04-08 12:04:08 +0200

    funzione calc_prob_tag

--- ProgettoMazzei/prova.py
+++ ProgettoMazzei/prova.py
@@ -2,8 +2,13 @@ from conllu import parse
 import numpy as np
 from collections import Counter
 
+
+
 #FUNZIONA
 #crea la matrice con tutte le parole e il corrispondente tag
+#[['Non' 'ADV']
+# .....
+# ['stelle' 'NOUN']]
 def create_mat(data):
     sentences = parse(data)
     mat = [[token["form"], token["upostag"]]    #aggiungi a mat la coppia
@@ -11,6 +16,7 @@ def create_mat(data):
            for token in s                       #per ogni token nella frase
            if token["upostag"] != '_']          #se il token è diverso da _
     return np.array(mat)
+
 
 
 #FUNZIONA
@@ -22,31 +28,66 @@ def calc_prob(data):
     freq = (counts / np.sum(counts)).reshape(-1, 1)
     return np.hstack((np_data_counts, freq))
 
+
+
 #FUNZIONA
 #conta quante volte compare un tag e ne calcola la probabilità a priori
+#[['ADV' '1039' '0.0855848434925865']
+# .....
+#['PART' '7' '0.0005766062602965404']]
 def count_tags(data):
     tags = [x[1] for x in data]
     return calc_prob(tags)
 
+
+
 #FUNZIONA
 #conta quante volte compare una parola e ne calcola la probabilità a priori
+#[['Non' '10' '0.0008237232289950577']
+# .....
+#'velle' '1' '8.237232289950576e-05']]
 def count_words(data):
     words = [x[0] for x in data]
     return calc_prob(words)
 
 
+
+#DOVREBBE FUNZIONARE
+#calcola le probabilità di ogni tag di essere associato ad una certa parola
+#[['ADV' 'Non' '10' '0.009624639076034648']
+#['ADV' 'qui' '17' '0.016361886429258902']
+# .....
+#['PART' 'Oh' '3' '0.42857142857142855']
+#['PART' 'O' '4' '0.5714285714285714']]
+def calc_prob_tag(data, count_tags):
+    result = np.empty((0, 4))
+
+    for tag in count_tags[:, 0]: # ciclo su tutti i tag
+
+        row_tag = data[data[:, 1] == tag] # prende tutte le occorrenze di tag
+        pairs = [tuple(x) for x in row_tag] # crea coppie (word, tag)
+        pairs_counts = Counter(pairs) # conta le occorrenze di ogni coppia
+        np_pairs_counts = np.array([[word, tag, count] for (tag, word), count in pairs_counts.items()]) # le converte in una lista numpy fatta: [word, tag, count]
+
+        total_tag = count_tags[count_tags[:, 0] == tag][0, 1].astype(int) # cerca in count_tags il tag e ritorna il numero totale di volte che compare
+
+        probability = np.array([[word, tag, int(count), int(count) / total_tag] for word, tag, count in np_pairs_counts]) #calcola la probabilità di tag di essere una certa parola e crea la lista: [word, tag, count, prob]
+        result = np.vstack((result, probability)) # aggiunge la lista al risultato
+
+    return result
+
+
+
 #SBAGLIATA
 #conta quante volte una parola è associata ad un tag
 #calcola la probabilità che una parola
-def count_word_tag_number(data):
-    pairs = [tuple(x) for x in data]  # (word, tag)
-    pairs_counts = Counter(pairs)
-    np_pairs_counts = np.array([[word, tag, count] for (word, tag), count in pairs_counts.items()])
-    counts = np_pairs_counts[:, 2].astype(int)
-    freq = (counts / np.sum(counts)).reshape(-1, 1)
-    return np.hstack((np_pairs_counts, freq))
-
-
+#def count_word_tag_number(data):
+#    pairs = [tuple(x) for x in data]  # (word, tag)
+#    pairs_counts = Counter(pairs)
+#    np_pairs_counts = np.array([[word, tag, count] for (word, tag), count in pairs_counts.items()])
+#    counts = np_pairs_counts[:, 2].astype(int)
+#    freq = (counts / np.sum(counts)).reshape(-1, 1)
+#    return np.hstack((np_pairs_counts, freq))
 
 
 
@@ -79,19 +120,13 @@ with open("UD_Italian-Old-master/it_old-ud-test.conllu", "r", encoding="utf-8") 
     data_old_test = f.read()
 old_test = create_mat(data_old_test)
 
+#[print(x) for x in old_test]
 #--------------------------------------------------------------------------------------------------------------------------------------
 
+count_tags = count_tags(old_test)
+count_words = count_words(old_test)
 
-
-test = count_word_tag_number(old_test)
-
-[print(x) for x in test]
-
-
-
-
-
-
+calc_prob_tag = calc_prob_tag(old_test, count_tags)
 
 
 
